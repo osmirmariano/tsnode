01167adf21b2488483ea4705ed2f7af92b012bf4
Osmir Mariano <osmirmariano@uft.edu.br>
2020-08-30 11:45:49 -0300

    Criando estrutura de projeto node com typescript

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import { User } from '../model/user'
+import { Request, Response } from 'express'
+
+class UserController {
+  public async index (req: Request, res: Response): Promise<Response> {
+    const users = await User.find()
+    return res.json(users)
+  }
+
+  public async create (req: Request, res: Response): Promise<Response> {
+    const user = await User.create(req.body)
+
+    return res.json(user)
+  }
+}
+
+export default new UserController()
